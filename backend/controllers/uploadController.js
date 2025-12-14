5cabdcc17a6271802ce545ecef42259ca357af94
kundan-thakur61 <152484837+kundan-thakur61@users.noreply.github.com>
2025-12-14 13:56:21 +0530

    Update uploadController.js

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -2,6 +2,7 @@
 const multer = require('multer');
 const path = require('path');
 const { uploadFromBuffer, uploadFromBase64, deleteImage } = require('../utils/cloudinary');
+const { uploadsDir } = require('../config/uploadsPath');
 const Product = require('../models/Product');
 
 /**
@@ -11,7 +12,7 @@ const Product = require('../models/Product');
  */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/uploads'));
+    cb(null, uploadsDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -85,7 +86,7 @@ async function uploadImage(req) {
       originalname: file.originalname,
       size: file.size,
       url: `/uploads/${file.filename}`,
-      localPath: path.join('public/uploads', file.filename)
+      localPath: path.join(uploadsDir, file.filename)
     };
   });
 
